e838dc6af36cfd55b161ce751bb9c207f1884624
Igli2 <sg.gerdon@gmx.de>
2022-03-12 15:13:16 +0100

    Changed PNG Decoder chunk decodingAdded time util

--- src/content/image/png_decoder.h
+++ src/content/image/png_decoder.h
@@ -5,6 +5,9 @@
 #include <string>
 #include <limits>
 #include <vector>
+#include <chrono>
+
+#include "../../util/time.h"
 
 class PNGDecoder {
     private:
@@ -15,10 +18,15 @@ class PNGDecoder {
         unsigned int img_compression_method;
         unsigned int img_filter_method;
         unsigned int img_interlace_method;
+        struct time last_edited;
 
         bool decode_signature(std::ifstream& image_stream);
         void decode_header(std::ifstream& image_stream);
+        void decode_time(std::ifstream& image_stream);
+        std::string get_chunk_name(std::ifstream& image_stream);
         unsigned int get_uint(std::ifstream& image_stream);
+        unsigned int get_byte(std::ifstream& image_stream);
+        unsigned short get_short(std::ifstream& image_stream);
     public:
         PNGDecoder();
         void decode(std::string file);
